b2badf0866e972070e5a83f8a2c7be7d9e5a6389
eumincavalcante <cavalcantee.yas@gmail.com>
2024-02-28 07:22:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,10 +25,14 @@ function gerarSequencia() {
 gerarSequencia();
 
 function adicionarNumero() {
-  let numeroEscolhido = document.querySelector('input').value;
-  listaNumerosEscolhidos.push(parseInt(numeroEscolhido));
-  limparCampo();
-  console.log(listaNumerosEscolhidos);
+  if (listaNumerosEscolhidos.length < 4) {
+    let numeroEscolhido = document.querySelector('input').value;
+    listaNumerosEscolhidos.push(parseInt(numeroEscolhido));
+    limparCampo();
+    exibirTexto('h4', `Números Escolhidos: ${listaNumerosEscolhidos}`);
+  } else {
+    document.getElementById('botaoAdicionar').setAttribute('disabled', true);
+  }
 }
 
 function limparCampo() {
@@ -38,30 +42,49 @@ function limparCampo() {
 
 let numerosCertos = 0;
 let numerosErrados = 0;
-let tentativas = 0;
 function compararListas() {
   for (i = 0; i < 4; i++) {
     if (listaNumerosGerados.indexOf(listaNumerosEscolhidos[i]) != -1 && listaNumerosGerados[i] == listaNumerosEscolhidos[i]) {
       numerosCertos++;
-      tentativas++;
     } else if (listaNumerosGerados.indexOf(listaNumerosEscolhidos[i]) != -1 && listaNumerosGerados[i] != listaNumerosEscolhidos[i]) {
       numerosErrados++;
-      tentativas++;
     }
   }
 }
 
 function verificarChute() {
   compararListas();
-  exibirTexto('h3', `Números Certos: ${numerosCertos}. Números Errados: ${numerosErrados}`);
+  if (numerosCertos == 4) {
+    exibirTexto('h3', `Parabéns! Você acertou!`);
+    exibirTexto('h5', `Número de Tentativas: ${tentativas}`);
+  } else {
+    exibirTexto('h3', `Números Certos: ${numerosCertos}. Números Errados: ${numerosErrados}`);
+    exibirTexto('h4', `Números Escolhidos: ${listaNumerosEscolhidos}`);
+    exibirTexto('h5', `Número de Tentativas: ${tentativas}`);
+  }
 }
 
 function novoJogo() {
   listaNumerosGerados = [];
+  listaNumerosEscolhidos = [];
   gerarSequencia();
   limparCampo();
   exibirTexto('h3', '');
+  exibirTexto('h4', '');
+  exibirTexto('h5', '');
   numerosCertos = 0;
   numerosErrados = 0;
   tentativas = 0;
+}
+
+let tentativas = 1;
+function novaTentativa() {
+  exibirTexto('h4', '');
+  exibirTexto('h5', '');
+  listaNumerosEscolhidos = [];
+  numerosCertos = 0;
+  numerosErrados = 0;
+  document.getElementById('botaoAdicionar').removeAttribute('disabled');
+  limparCampo();
+  tentativas++;
 }
